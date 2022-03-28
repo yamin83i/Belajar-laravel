b05d062868b034db13ee716bc87ec7dd10569df5
yamin83i <mashabulyamin99@gmail.com>
2022-03-29 06:39:09 +0700

    fourth commit

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -13,6 +13,21 @@
             my Post
           </a>
         </li> 
-      </ul>      
+      </ul>
+
+    @can('admin')
+          
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <span>Administrator</span>
+      </h6>
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+            <span data-feather="grid"></span>
+            Post Category
+          </a>
+        </li>
+      </ul>
+    @endcan
     </div>
   </nav>
